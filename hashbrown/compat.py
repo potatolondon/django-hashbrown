6e1ee8cc2bc195b8fca020073542ba0487ccdc92
Pablo Recio <pablo@potatolondon.com>
2014-10-03 10:24:25 +0100

    More clear comment on compat.py

--- hashbrown/compat.py
+++ hashbrown/compat.py
@@ -2,7 +2,11 @@ import django
 
 __all__ = ['User', 'get_user_model']
 
-# Django 1.5+ compatibility
+
+# This small file just keeps the app compatible with Django versions 1.5+ and
+# < 1.5. There are some "unused" imports like `get_user_model` or `User` but
+# they are ther to get imported like `from .compat import get_user_model`
+
 if django.VERSION >= (1, 5):
     from django.conf import settings
     from django.contrib.auth import get_user_model
