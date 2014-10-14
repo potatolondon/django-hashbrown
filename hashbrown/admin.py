8845482de635a44c768ea80a7f8dfed1d1c3ee56
David Buxton <david@gasmark6.com>
2014-10-14 17:33:07 +0100

    Show more information in the Django admin list display view.

--- hashbrown/admin.py
+++ hashbrown/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Switch
 
-admin.site.register(Switch)
+
+class SwitchAdmin(admin.ModelAdmin):
+    list_display = ('label', 'globally_active', 'description')
+
+
+admin.site.register(Switch, SwitchAdmin)
