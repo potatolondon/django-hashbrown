922dce5895c038b722c64808c72376934ebc83a9
David Buxton <david@gasmark6.com>
2014-10-17 12:16:39 +0100

    Make it super easy to edit switches in the admin list display.

--- hashbrown/admin.py
+++ hashbrown/admin.py
@@ -5,6 +5,7 @@ from .models import Switch
 
 class SwitchAdmin(admin.ModelAdmin):
     list_display = ('label', 'globally_active', 'description')
+    list_editable = ('globally_active',)
 
 
 admin.site.register(Switch, SwitchAdmin)
