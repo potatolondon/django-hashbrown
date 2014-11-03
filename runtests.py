24077ffc87dffd4813b5e9336f1d94f4ba3d5cfb
David Buxton <david@gasmark6.com>
2014-11-03 22:16:29 +0000

    Removed duplicate Python environments from Travis, tweak test runner so we don't get any warnings.

--- runtests.py
+++ runtests.py
@@ -5,20 +5,21 @@ import django
 from django.conf import settings
 
 DIRNAME = os.path.dirname(__file__)
-settings.configure(DEBUG = True,
-                   DATABASES={
-                        'default': {
-                              'ENGINE': 'django.db.backends.sqlite3',
-                              'ATOMIC_REQUESTS': True,
-                        }
-                  },
-                   INSTALLED_APPS = ('django.contrib.auth',
-                                     'django.contrib.contenttypes',
-                                     'django.contrib.sessions',
-                                     'django.contrib.admin',
-                                     'hashbrown',
-                                    )
-                   )
+settings.configure(
+      DEBUG = True,
+      DATABASES = {
+            'default': {'ENGINE': 'django.db.backends.sqlite3'},
+      },
+      INSTALLED_APPS = (
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.admin',
+            'hashbrown',
+      ),
+      # Django 1.7 raises a warning if this isn't set. Pollutes test output.
+      MIDDLEWARE_CLASSES = (),
+)
 
 try:
       django.setup()
@@ -26,8 +27,13 @@ except AttributeError:
       # Running Django<1.7
       pass
 
-from django.test.simple import DjangoTestSuiteRunner
-test_runner = DjangoTestSuiteRunner(verbosity=1)
+try:
+      from django.test.runner import DiscoverRunner as TestSuiteRunner
+except ImportError:
+      # Running Django<1.6
+      from django.test.simple import DjangoTestSuiteRunner as TestSuiteRunner
+
+test_runner = TestSuiteRunner(verbosity=1)
 failures = test_runner.run_tests(['hashbrown', ])
 if failures:
     sys.exit(failures)
