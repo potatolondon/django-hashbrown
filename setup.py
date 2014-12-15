9acc3ff6accad6a34174e273057ed3888088da75
Pablo Recio <pablo@potatolondon.com>
2014-12-15 15:05:52 +0000

    Bumps version nomber

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
 
 setup(
     name="django-hashbrown",
-    version="0.6",
+    version="0.6.1",
     author="Pablo Recio",
     author_email="pablo@potatolondon.com",
     description="Yet another dead simple feature switching library for Django.",
